06b4e601c9332bb7c4b814e05cf4312127778383
Christian Böhme <christian.boehme.business@gmail.com>
2024-01-07 23:32:06 +0100

    script

---
+++ script.js
@@ -0,0 +1,44 @@
+// Elemente auswählen
+const mainMenu = document.getElementById("as-navbar-main-collapse");
+const leftMainMenu = mainMenu.querySelector("ul.navbar-nav");
+
+// Neue Elemente erstellen
+const liToolMenu = document.createElement("li");
+const aManagerLounge = document.createElement("a");
+const ulToolMenu = document.createElement("ul");
+const liDropdown = document.createElement("li");
+const aAbout = document.createElement("a");
+const spanCaret = document.createElement("span");
+
+// Text für die neuen Links festlegen
+aManagerLounge.textContent = "Manager Lounge";
+aAbout.textContent = "About";
+
+// Klassen für Bootstrap hinzufügen
+liToolMenu.classList.add("nav-item", "dropdown");
+aManagerLounge.classList.add("nav-link", "dropdown-toggle");
+ulToolMenu.classList.add("dropdown-menu");
+liDropdown.classList.add("nav-item");
+
+// Attribut für Bootstrap hinzufügen
+aManagerLounge.setAttribute("href", "#");
+aManagerLounge.setAttribute("role", "button");
+aManagerLounge.setAttribute("data-toggle", "dropdown");
+aManagerLounge.setAttribute("aria-haspopup", "true");
+aManagerLounge.setAttribute("aria-expanded", "false");
+
+// Inhalt für das Dropdown-Menü
+spanCaret.classList.add("caret");
+aManagerLounge.appendChild(spanCaret);
+
+// Link für "About" dem Dropdown-Menü hinzufügen
+aAbout.setAttribute("href", "about.html"); // Hier der relative Pfad zur about.html im Wurzelverzeichnis der Erweiterung
+liDropdown.appendChild(aAbout);
+
+// Links und Dropdown-Menü dem Listenelement hinzufügen
+liToolMenu.appendChild(aManagerLounge);
+ulToolMenu.appendChild(liDropdown);
+liToolMenu.appendChild(ulToolMenu);
+
+// Listenelement dem Hauptmenü hinzufügen
+leftMainMenu.appendChild(liToolMenu);
